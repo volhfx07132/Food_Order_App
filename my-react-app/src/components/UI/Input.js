24bcdcd9eb02a8e4853661841f04800212b485b7
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-05 20:01:19 +0700

    Working on the Shopping Cart Component

--- my-react-app/src/components/UI/Input.js
+++ my-react-app/src/components/UI/Input.js
@@ -1,11 +1,11 @@
 import React from "react";
-import classes from "./Input.module.css";
+import classes from './Input.module.css';
 
 const Input = (props) => {
   return (
     <div className={classes.input}>
-      <label httmFor={props.input.id}>{props.label}</label>
-      <input {...props.input}/>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
     </div>
   );
 };
