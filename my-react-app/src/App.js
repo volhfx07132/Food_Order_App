f51e4927543e77edd4c70c555eb42c9f94f221ec
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-05 16:54:45 +0700

    First commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Let's get started</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
